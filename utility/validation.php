3b914b33344813f4b712085db8e2aabe981e784c
Angie <angelitoregero@gmail.com>
2025-07-15 12:15:02 +0800

    test

--- utility/validation.php
+++ utility/validation.php
@@ -1,3 +1,12 @@
 <!-- ========== Validation for all fieldsets ========== -->
+<?php
 
+    $email = "/sampleemail@gmail.com/i";
+    function validateEmail(){
+    //this function checks if the inputted email is valid.
+        
+    }
+
+
+?>
 <!-- ========== Email, Password, Positions, Party name (like 5letters more, bawal numbers ganun), Candidate name (letters only) ========== -->
