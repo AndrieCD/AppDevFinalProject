0c816f719a8b0e112d2ca7ab010abc32973fbb73
Lee <lightninggirl2234@gmail.com>
2025-07-15 12:38:37 +0800

    this is message

--- admin/index.php
+++ admin/index.php
@@ -1,13 +1,8 @@
 <!-- ========== INDEX ========== -->
 <!-- ========== HomePage/Landing Page after mag login ========== -->
 
-<<<<<<< HEAD
-<!-- ========== THIS IS LEO ========== -->
- <!-- ========== THIS IS LEO ========== -->
-=======
 <?php
 
 // this is me this is real <33
 
 ?>
->>>>>>> 9fbf8d4e073dbb959a21726ed8ebb3dfc4e807a4
