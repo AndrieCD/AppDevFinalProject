a549af920c5d0d11b008e70d67e8df63528d9fe5
ANGEL JANICA FABREGAS <202311675@fit.edu.ph>
2025-07-16 09:10:54 +0800

    Added student login interface

--- student/login.php
+++ student/login.php
@@ -1,3 +1,23 @@
 <!-- ========== LOGIN | SAME LAYOUT ITSURA BASTA========== -->
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Student Login</title>
+</head>
+<body>
+    <h2>Student Login</h2>
+    <form action="#" method="post">
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
+
 <!-- ========== EMAIL/PASSWORD for ADMIN ========== -->
